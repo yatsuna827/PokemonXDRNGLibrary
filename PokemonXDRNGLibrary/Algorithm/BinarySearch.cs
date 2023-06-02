bcb5e2e9b88bb817b424951a1b027c23e4192fc2
niart120 <38847256+niart120@users.noreply.github.com>
2023-06-02 20:08:36 +0900

    Create BinarySearch.cs
    SeedFinder.csへの新規依存ファイル

---
+++ PokemonXDRNGLibrary/Algorithm/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonXDRNGLibrary
+{
+    static class BinarySearch
+    {
+        // 参考: https://qiita.com/hamko/items/794a92c456164dcc04ad
+        // item < list[i]となる最小のiを返す.
+        internal static int UpperBound(this uint[] array, uint item)
+        {
+            var ng = -1;
+            var ok = array.Length;
+
+            while (ok - ng > 1)
+            {
+                var mid = (ng + ok) / 2;
+                if (item < array[mid]) ok = mid; else ng = mid;
+            }
+
+            return ok == array.Length ? -1 : ok;
+        }
+
+        // item <= list[i]となる最小のiを返す.
+        internal static int LowerBound(this uint[] array, uint item)
+        {
+            var ng = -1;
+            var ok = array.Length;
+
+            while (ok - ng > 1)
+            {
+                var mid = (ng + ok) / 2;
+                if (item <= array[mid]) ok = mid; else ng = mid;
+            }
+
+            return ok == array.Length ? -1 : ok;
+        }
+    }
+}
